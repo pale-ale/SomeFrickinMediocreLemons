9de2dd7bd4e6238c287a3c5a33eeab59a1e17eb7
pale-ale <alexlol22@aol.com>
2021-03-02 10:52:01 +0100

    add costdisplay to preview

--- UI/CardPreview.h
+++ UI/CardPreview.h
@@ -26,14 +26,41 @@ public:
         cardImage.setTexture(*(cardToPreview.getTexture().get()), true);
         cardImage.setScale(2.5,2.5);
         cardImage.setOrigin({cardImage.getLocalBounds().width/2, cardImage.getLocalBounds().height/2});
+        FMana cardCost = cardToPreview.cost;
+        costDisplayRed.setFillColor(Settings::RedColor);
+        costDisplayRed.setString(to_string(cardCost.Red));
+        costDisplayRed.setFont(font);
+        costDisplayRed.setScale(0.5, 0.5);
+        costDisplayGreen.setFillColor(Settings::GreenColor);
+        costDisplayGreen.setString(to_string(cardCost.Green));
+        costDisplayGreen.setFont(font);
+        costDisplayGreen.setScale(0.5, 0.5);
+        costDisplayBlue.setFillColor(Settings::BlueColor);
+        costDisplayBlue.setString(to_string(cardCost.Blue));
+        costDisplayBlue.setFont(font);
+        costDisplayBlue.setScale(0.5, 0.5);
+        costDisplayBlack.setFillColor(Settings::BlackColor);
+        costDisplayBlack.setString(to_string(cardCost.Black));
+        costDisplayBlack.setFont(font);
+        costDisplayBlack.setScale(0.5, 0.5);
+        costDisplayWhite.setFillColor(Settings::WhiteColor);
+        costDisplayWhite.setString(to_string(cardCost.White));
+        costDisplayWhite.setFont(font);
+        costDisplayWhite.setScale(0.5, 0.5);
     }
     const sf::Vector2f titlePos = {Settings::defaultWidth/2, (int)Settings::defaultHeight/2 - 110};
+    const sf::Vector2f costPos = {Settings::defaultWidth/2 + 70, (int)Settings::defaultHeight/2 - 38};
     const sf::Vector2f backgroundPos = {Settings::defaultWidth/2, Settings::defaultHeight/2};
     const sf::Vector2f imagePos = {Settings::defaultWidth/2, Settings::defaultHeight/2-30};
     const sf::Vector2f descriptionPos = {Settings::defaultWidth/2, Settings::defaultHeight/2 + 25};
     const sf::Vector2f size = {200, 350};
     sf::Font font;
     sf::Text cardTitle;
+    sf::Text costDisplayRed;
+    sf::Text costDisplayGreen;
+    sf::Text costDisplayBlue;
+    sf::Text costDisplayBlack;
+    sf::Text costDisplayWhite;
     sf::Text description;
     sf::Sprite cardImage;
     sf::RectangleShape background;
@@ -44,6 +71,11 @@ public:
         background.setPosition(backgroundPos);
         cardImage.setPosition(imagePos);
         description.setPosition(descriptionPos);
+        costDisplayRed.setPosition(costPos + sf::Vector2f{0, -30});
+        costDisplayGreen.setPosition(costPos + sf::Vector2f{0, -15});
+        costDisplayBlue.setPosition(costPos + sf::Vector2f{0, 0});
+        costDisplayBlack.setPosition(costPos + sf::Vector2f{0, 15});
+        costDisplayWhite.setPosition(costPos + sf::Vector2f{0, 30});
     }
 
 protected:
@@ -54,5 +86,10 @@ protected:
         target.draw(cardImage);
         target.draw(description);
         target.draw(cardTitle);
+        target.draw(costDisplayRed);
+        target.draw(costDisplayGreen);
+        target.draw(costDisplayBlue);
+        target.draw(costDisplayBlack);
+        target.draw(costDisplayWhite);
     }
 };
